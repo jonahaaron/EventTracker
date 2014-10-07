9ff5088de784ee426792ab935467f7c11c7a4e17
jonahaaron <jah132000@gmail.com>
2014-10-07 14:11:47 -0500

    Replaced unsafe secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,11 +73,11 @@ Rails.application.configure do
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
