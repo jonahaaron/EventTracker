c4df64bc7eb4b0ff6a29d03f3c1a054aa2cc98c9
jonahaaron <jah132000@gmail.com>
2014-10-07 15:26:11 -0500

    Added guest log-in functionality

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,11 @@
 class VisitorsController < ApplicationController
+  before_filter :authenticate
+  
+  def authenticate
+    unless current_user
+        authenticate_or_request_with_http_basic('Administration') do |username, password|
+          username == ENV["GUEST_LOGIN"] && password == ENV["GUEST_PASSWORD"]
+      end
+    end
+  end
 end
